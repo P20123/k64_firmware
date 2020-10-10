90bac923e28faf3e61c9fd40ebdfedebde98d861
Piers Kwan <pik3387@rit.edu>
2020-10-10 11:52:30 -0400

    Working Asynchronous I2C Driver (#7)
    * Working I2C header and source file.
    
    * Fixed problems mentioned in the PR review.

---
+++ include/drivers/kinetis/i2c.h
@@ -0,0 +1,122 @@
+#pragma once
+#include <MK64F12.h>
+#include <stdbool.h>
+
+/*************
+ * CONSTANTS
+ *************/
+/* Number of devices to configure (create global contexts for). */
+#define I2C_NUM_DEVICES 1
+
+/* Constants used to specify the status of the I2C channel */
+#define I2C_AVAILABLE 0
+#define I2C_BUSY 1
+#define I2C_ERROR 2
+
+/* Constants used to specify the mode of the I2C channel. */
+#define I2C_WRITING 0
+#define I2C_READING 1
+
+/**********
+ * MACROS
+ **********/
+
+/* Address macros specify a device address in read or write mode */
+#define I2C_WRITE_ADDR(x)   (i2c_seq_t){.opcode=I2C_WRITE, .addr=(x << 1)}
+#define I2C_READ_ADDR(x)    (i2c_seq_t){.opcode=I2C_WRITE, .addr=(x << 1) | 1}
+
+/* These macros are essentially identical, they send data down the line by
+   setting the data field of the sequence struct. */
+#define I2C_WRITE_REG(x)    (i2c_seq_t){.opcode=I2C_WRITE, .reg=x}
+#define I2C_WRITE_VALUE(x)  (i2c_seq_t){.opcode=I2C_WRITE, .value=x}
+
+/* These macros do not use the data field of the struct and simply instruct the
+   driver to expect a read or send a restart, respectively. */
+#define I2C_SEND_READ       (i2c_seq_t){.opcode=I2C_READ}
+#define I2C_SEND_RESTART    (i2c_seq_t){.opcode=I2C_RESTART}
+
+/* This macro blocks until the I2C transaction in the specified channel
+   is complete. Used in I2C device initialization. */
+#define I2C_TX_WAIT(x) while(i2c_chs[x].status != I2C_AVAILABLE);
+
+/*************
+ * DATATYPES
+ *************/
+
+/* I2C Configuration struct. */
+typedef struct {
+    uint8_t i2c_num;        /* number I2C device to be configured */
+    uint32_t baud;          /* desired baud rate */
+    bool stophold;          /* finish sending messages before I2C shutdown */
+    bool interrupt;         /* configures interrupts -> always use true */
+    uint8_t filter_clocks;  /* 0-15 for number of clocks */
+    uint8_t priority;       /* interrupt priority */
+} i2c_config_t;
+
+/* I2C Sequence struct. A sequence consists of an array of these structs. */
+typedef struct {
+    enum opcode_t {
+        I2C_RESTART,
+        I2C_READ,
+        I2C_WRITE
+    } opcode;               /* I2C operation to perform */
+    union {
+        uint8_t addr;
+        uint8_t reg;
+        uint8_t value;
+    };                      /* union for the data for clarity */
+} i2c_seq_t;
+
+/* I2C Channel state struct. Keeps track of the state of each channel. */
+typedef struct {
+    i2c_seq_t *seq;             /* pointer to the sequence to send */
+    i2c_seq_t *seq_end;         /* pointer to the end of the sequence */
+    uint8_t *received_data;     /* pointer to the rx data buffer */
+    uint8_t reads_ahead;        /* number of reads remaining */
+    uint8_t txrx;               /* I2C_READING or I2C_WRITING */
+    uint8_t status;             /* status of the channel: AVAILABLE, BUSY, or ERROR */
+    void (*callback)(void *);   /* pointer to the callback function */
+    void *args;                 /* pointer to the callback function args */
+} I2C_Channel;
+
+/*************
+ * VARIABLES
+ *************/
+
+/* Global variable that keeps track of the I2C Channel states. */
+extern volatile I2C_Channel i2c_chs[I2C_NUM_DEVICES];
+
+/*************
+ * FUNCTIONS
+ *************/
+
+/**
+ * Initializes the I2C peripheral. Interrupts and master mode are enabled by
+ *      send sequence.
+ * @param config A configuration struct for the I2C peripheral.
+ * @param clk_f_hz The frequency of the clock going to the I2C channel.
+ *                 Usually the bus clock speed.
+ */
+void i2c_init(i2c_config_t config, uint32_t clk_f_hz);
+
+/**
+ * Sends a sequence using the specified I2C module.
+ * @param ch_num I2C channel number to send a sequence.
+ * @param seq A pointer to the sequence to be sent.
+ * @param seq_len Length of the sequence to be sent (number of bytes).
+ * @param received_data A pointer to where data recieved by the sequence will go.
+ *                      There is no runoff protection.
+ * @param callback A pointer to a callback function to be run at the conclusion of
+ *                 sending a sequence.
+ * @param args Arguments for the callback function.
+ * @return 0 if no error, -1 if I2C is busy or arbitration is lost.
+ */
+uint32_t i2c_send_seq(uint32_t ch_num, i2c_seq_t *seq, uint32_t seq_len, uint8_t *received_data, void (*callback)(void *), void *args);
+
+/**
+ * Generic IRQ handler that all I2C ISRs will call.
+ * @param ch_num I2C channel number that was interrupted.
+ * @return 0 if no error, -1 if there is an I2C error,
+ *         -2 if interrupt wasn't triggered but IRQ was entered.
+ */
+uint32_t i2c_irq_handler(uint8_t ch_num);
---
+++ src/drivers/kinetis/i2c.c
@@ -0,0 +1,308 @@
+#include <MK64F12.h>
+#include <drivers/i2c.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+/* SCL clock divider table for baudrate calculation */
+static const uint16_t SCL_LUT[] = {
+    20,  22,  24,  26,   28,   30,   34,   40,   28,   32,   36,   40,   44,   48,   56,   68,
+    48,  56,  64,  72,   80,   88,   104,  128,  80,   96,   112,  128,  144,  160,  192,  240,
+    160, 192, 224, 256,  288,  320,  384,  480,  320,  384,  448,  512,  576,  640,  768,  960,
+    640, 768, 896, 1024, 1152, 1280, 1536, 1920, 1280, 1536, 1792, 2048, 2304, 2560, 3072, 3840};
+
+/* global variable to store the channel parameters */
+volatile I2C_Channel i2c_chs[I2C_NUM_DEVICES];
+
+/* global variable to store the base pointers */
+static I2C_Type *i2c_bases[] = I2C_BASE_PTRS;
+
+void i2c_init(i2c_config_t config, uint32_t clk_f_hz) {
+    /* get the base pointer */
+    I2C_Type *base = i2c_bases[config.i2c_num];
+
+    /* get the irq pointers */
+    uint32_t i2c_irqs[] = I2C_IRQS;
+
+    /* route clocks to I2C module */
+    switch(config.i2c_num) {
+        case 0:
+            SIM->SCGC4 |= SIM_SCGC4_I2C0_MASK;
+            break;
+        case 1:
+            SIM->SCGC4 |= SIM_SCGC4_I2C1_MASK;
+            break;
+        case 2:
+            SIM->SCGC1 |= SIM_SCGC1_I2C2_MASK;
+            break;
+    }
+
+    /* reset the module */
+    base->A1 = 0;
+    base->F = 0;
+    base->C1 = 0;
+    base->S = 0xFFu;
+    base->C2 = 0;
+    base->RA = 0;
+
+    /* disable i2c for config and clear all status flags */
+    base->C1 &= ~I2C_C1_IICEN_MASK;
+    base->S = I2C_S_ARBL_MASK | I2C_S_IICIF_MASK;
+
+    /* set baud rate with least error */
+    uint32_t rate;
+    uint32_t abserr;
+    uint32_t besterr = UINT32_MAX;
+    uint8_t bestmult, besticr;
+    for(uint8_t mult = 0; mult <= 2; mult++) {
+        for(uint8_t icr = 0; icr < sizeof(SCL_LUT) / sizeof(uint16_t); icr++) {
+            rate = clk_f_hz / (SCL_LUT[icr] << mult);
+            abserr = config.baud > rate ? config.baud - rate : rate - config.baud;
+
+            if(abserr < besterr) {
+                if(0 == abserr) {
+                    break;
+                }
+                bestmult = mult;
+                besticr = icr;
+                besterr = abserr;
+            }
+        }
+        if (0 == besterr) {
+            break;
+        }
+    }
+    base->F = I2C_F_MULT(bestmult) | I2C_F_ICR(besticr);
+
+    /* configure stop-hold enable */
+    if(true == config.stophold) {
+        base->FLT |= I2C_FLT_SHEN_MASK;
+    }
+    else {
+        base->FLT &= I2C_FLT_SHEN_MASK;
+    }
+
+    /* configure glitch filter */
+    base->FLT |= I2C_FLT_FLT(config.filter_clocks);
+
+    /* enable interrupts */
+    if (true == config.interrupt) {
+        asm("cpsid i");
+        NVIC_EnableIRQ(i2c_irqs[config.i2c_num]);
+        NVIC->IP[i2c_irqs[config.i2c_num]] = config.priority;
+        asm("cpsie i");
+    }
+
+    /* enable i2c */
+    base->C1 |= I2C_C1_IICEN_MASK;
+
+    /* initialize the channel to be available */
+    i2c_chs[config.i2c_num].status = I2C_AVAILABLE;
+}
+
+uint32_t i2c_send_seq(uint32_t ch_num, i2c_seq_t *seq, uint32_t seq_len, uint8_t *received_data, void (*callback)(void *), void *args) {
+    /* get the channel and the base from the globals */
+    volatile I2C_Channel *ch = &i2c_chs[ch_num];
+    I2C_Type *base = (I2C_Type *)i2c_bases[ch_num];
+
+    /* initialize the errno to zero */
+    uint8_t errno = 0;
+
+    /* error out if the peripheral is busy */
+    if(I2C_BUSY == ch->status) {
+        errno = -1;
+        goto exit;
+    }
+
+    /* initializing transfer */
+    ch->seq = seq;
+    ch->seq_end = seq + seq_len;
+    ch->received_data = received_data;
+    ch->status = I2C_BUSY;
+    ch->txrx = I2C_WRITING;
+    ch->callback = callback;
+    ch->args = args;
+
+    /* clear IICIF by writing a 1 to it */
+    base->S |= I2C_S_IICIF_MASK;
+
+    /* enable interrupts */
+    base->C1 = (I2C_C1_IICEN_MASK | I2C_C1_IICIE_MASK);
+
+    /* generate a start condition and set to transmit */
+    base->C1 |= (I2C_C1_MST_MASK | I2C_C1_TX_MASK);
+
+    if(base->S & I2C_S_ARBL_MASK) {
+        errno = -1;
+        goto i2c_send_seq_cleanup;
+    }
+
+    /* write the address */
+    base->D = (*ch->seq).addr;
+    ch->seq++;
+    goto exit;
+
+i2c_send_seq_cleanup:
+    /* generate a STOP, disable interrupts, change to RX */
+    base->C1 &= ~(I2C_C1_MST_MASK | I2C_C1_IICIE_MASK | I2C_C1_TX_MASK);
+    ch->status = I2C_ERROR;
+
+exit:
+    return errno;
+}
+
+uint32_t i2c_irq_handler(uint8_t ch_num) {
+#ifdef __KINETIS_I2C_BLOCKING_ISR
+    asm("cpsid i");
+#endif
+    /* get the channel and the base from the globals */
+    volatile I2C_Channel *ch = &i2c_chs[ch_num];
+    I2C_Type *base = (I2C_Type *)i2c_bases[ch_num];
+
+    /* initialize errno to zero */
+    int8_t errno = 0;
+
+    /* ensure that irq was from the current I2C module */
+    if(!(base->S & I2C_S_IICIF_MASK)) {
+        errno = -2;
+        goto i2c_isr_exit;
+    }
+
+    /* clear IICIF by writing a 1 to it */
+    I2C0->S |= I2C_S_IICIF_MASK;
+    
+    /* check if arbitration lost */
+    if(base->S & I2C_S_ARBL_MASK) {
+        base->S |= I2C_S_ARBL_MASK;
+        goto i2c_isr_error;
+    }
+
+    /* handle reading */
+    if(ch->txrx == I2C_READING) {
+        switch(ch->reads_ahead) {
+            case 0:
+                /* switch to tx to avoid another read or send restart */
+                base->C1 |= I2C_C1_TX_MASK;
+
+                /* perform the final read */
+                *ch->received_data++ = base->D;
+
+                /* if repeated start */
+                if((ch->seq < ch->seq_end) && ((*ch->seq).opcode == I2C_RESTART)) {
+                    /* generate a repeated start! */
+                    base->C1 |= I2C_C1_RSTA_MASK;
+
+                    /* address write post repeated start */
+                    ch->txrx = I2C_WRITING;
+                    ch->seq++;
+                    base->D = (*ch->seq).addr;
+                }
+                /* no repeated start */
+                else {
+                    goto i2c_isr_stop;
+                }
+                break;
+            case 1:
+                /* do not ack the final read */
+                base->C1 |= I2C_C1_TXAK_MASK;
+                *ch->received_data++ = base->D;
+                break;
+            default:
+                /* simply perform a read */
+                *ch->received_data++ = base->D;
+                break;
+        }
+        /* decrement the number of reads */
+        ch->reads_ahead--;
+    }
+    /* handle writing */
+    else {
+        /* check if sequence end */
+        if(ch->seq == ch->seq_end) {
+            goto i2c_isr_stop;
+        }
+
+        /* check if NACK */
+        if(base->S & I2C_S_RXAK_MASK) {
+            goto i2c_isr_error;
+        }
+
+        /* check if we have a restart */
+        if((*ch->seq).opcode == I2C_RESTART) {
+            /* generate repeated start and ensure TX */
+            base->C1 |= (I2C_C1_RSTA_MASK | I2C_C1_TX_MASK);
+
+            /* address write post repeated start */
+            ch->seq++;
+            base->D = (*ch->seq).addr;
+        }
+        /* not a repeated start */
+        else {
+            /* handle a read */
+            if((*ch->seq).opcode == I2C_READ) {
+                /* set channel status to read */
+                ch->txrx = I2C_READING;
+
+                /* at least one read ahead */
+                ch->reads_ahead = 1;
+
+                /* parse the number of reads */
+                while(((ch->seq + ch->reads_ahead) < ch->seq_end) &&
+                        ((*(ch->seq + ch->reads_ahead)).opcode == I2C_READ)) {
+                    ch->reads_ahead++;
+                }
+
+                /* switch to rx */
+                base->C1 &= ~I2C_C1_TX_MASK;
+
+                if(ch->reads_ahead == 1) {
+                    /* do not ACK the final read */
+                    base->C1 |= I2C_C1_TXAK_MASK;
+                }
+                else {
+                    /* ACK every other read */
+                    base->C1 &= ~(I2C_C1_TXAK_MASK);
+                }
+
+                /* dummy read without incrementing received data pointer */
+                *ch->received_data = base->D;
+                ch->reads_ahead--;
+            }
+            /* handle a write */
+            else {
+                base->D = (*ch->seq).value;
+            }
+        }
+    }
+    ch->seq++;
+    goto i2c_isr_exit;
+
+i2c_isr_stop:
+    /* generate a STOP, switch to RX, disable interrupts */
+    base->C1 &= ~(I2C_C1_MST_MASK | I2C_C1_TXAK_MASK | I2C_C1_IICIE_MASK);
+
+    /* call the callback function on success if it exists */
+    if(ch->callback) {
+        (*ch->callback)(ch->args);
+    }
+
+    /* set channel back to available */
+    ch->status = I2C_AVAILABLE;
+    goto i2c_isr_exit;
+
+i2c_isr_error:
+    /* generate a STOP and disable interrupts */
+    base->C1 &= ~(I2C_C1_MST_MASK | I2C_C1_IICIE_MASK);
+    ch->status = I2C_ERROR;
+    errno = -1;
+    goto i2c_isr_exit;
+
+i2c_isr_exit:
+    return errno;
+#ifdef __KINETIS_I2C_BLOCKING_ISR
+    asm("cpsie i");
+#endif
+}
+
+void I2C0_IRQHandler() {
+    i2c_irq_handler(0);
+}
