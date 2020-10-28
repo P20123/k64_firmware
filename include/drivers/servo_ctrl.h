a7b078f415d36274781e4deb3c1e2a2a2ef99f88
sjl2178 <sjl2178@g.rit.edu>
2020-10-28 12:47:17 -0400

    added second servo pwm

--- include/drivers/servo_ctrl.h
+++ include/drivers/servo_ctrl.h
@@ -1,5 +1,6 @@
 #pragma once
 
-void servo_set_angle(int16_t angle);
+void servo_set_rightangle(int16_t angle);
+void servo_set_leftangle(int16_t angle);
 void servo_init_io();
 void servo_init_pwm();
