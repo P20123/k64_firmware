d1787402650bd768892b3dc441feb13caf8c60d7
Piers Kwan <pik3387@rit.edu>
2020-10-29 00:11:16 -0400

    working filter with multitasking

--- include/environment.h
+++ include/environment.h
@@ -1,8 +1,13 @@
 #pragma once
 #include <drivers/kinetis/uart.h>
+#include <drivers/kinetis/i2c.h>
 /**
  * The real thing is in src/kernel/environment.c
  */
 extern uart_config uart0_conf;
 extern uart_config uart3_conf;
+
+/* i2c config structs */
+extern i2c_config_t i2c0_conf;
+
 extern int uart0_fileno;
--- src/kernel/environment.c
+++ src/kernel/environment.c
@@ -1,15 +1,16 @@
 #include <environment.h>
 #include <drivers/kinetis/uart.h>
+#include <drivers/kinetis/i2c.h>
 
 // UART 0 CONFIGURATION (OpenSDA UART <-> USB)
 uart_config uart0_conf = {
     .uart_base = UART0,
-    .rx_pcr = &(PORTB->PCR[16]),
-    .tx_pcr = &(PORTB->PCR[17]),
+    .rx_pcr = &(PORTA->PCR[1]),
+    .tx_pcr = &(PORTA->PCR[2]),
     .rt_pcr = 0,
     .ct_pcr = 0,
-    .rx_alt = 3,
-    .tx_alt = 3,
+    .rx_alt = 2,
+    .tx_alt = 2,
     .rt_alt = 255,
     .ct_alt = 255,
     .baud = 115200,
@@ -17,7 +18,7 @@ uart_config uart0_conf = {
     .uart_clock_gate_base = &(SIM->SCGC4),
     .port_clock_gate_base = &(SIM->SCGC5),
     .uart_clock_gate_mask = SIM_SCGC4_UART0_MASK,
-    .port_clock_gate_mask = SIM_SCGC5_PORTB_MASK,
+    .port_clock_gate_mask = SIM_SCGC5_PORTA_MASK,
     .configure_interrupts = 1,
     .irqn = UART0_RX_TX_IRQn,
     .priority = 0,
@@ -47,6 +48,15 @@ uart_config uart3_conf = {
     .priority = 1,
     .rwfifo_sz = 0,
     .twfifo_sz = 0
+};
+
+i2c_config_t i2c0_conf = {
+    .i2c_num = 0,
+    .baud = 90000,
+    .stophold = true,
+    .interrupt = true,
+    .filter_clocks = 0,
+    .priority = 0
 };
 
 int uart0_fileno;
