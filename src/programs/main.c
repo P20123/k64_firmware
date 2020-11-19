f4ee58e1ce53a86cebda2d9c4b16804009d34c5f
Andy <PyroAVR@users.noreply.github.com>
2020-11-19 17:01:19 -0500

    Added XPC Relay on UART0 as a process. (#20)
    The XPC Relay is initialized on UART0 at system startup.
    Processes which need to communicate via XPC should first call
    `xpc_yield_until_init()` to force them to yield until the relay is
    ready.
    
    Other processes writing to UART0 directly should avoid this behavior, as
    it will interleave the XPC stream with raw data, which may be
    interpreted as XPC messages.  Instead, reserving a "from" channel for
    that process' output is advised.

--- src/programs/main.c
+++ src/programs/main.c
@@ -8,8 +8,10 @@
 #include <kernel/private/static_memory.h>
 #include <kernel/schedule.h>
 #include <drivers/arm/cm4/systick.h>
-#include <programs/sensor_read.h>
 #include <drivers/devices/status_leds.h>
+#include <programs/sensor_read.h>
+#include <programs/xpc_relay_event_loop.h>
+#include <programs/xpc_spammer.h>
 
 /**
  * Entry point for the process switcher.
@@ -19,6 +21,12 @@ int init_main(void) {
     process_init(&sensor_read_app, &sensor_read_main, sensor_read_stack,
                  SENSOR_READ_STACK_SIZE, true, false);
     schedule_process(&process_table, &sensor_read_app);
+#endif
+
+#ifdef TASK_EN_XPC_RELAY
+    process_init(&xpc_relay_event_loop_app, &xpc_relay_event_loop_main,
+            xpc_relay_event_loop_stack, XPC_RELAY_STACK_SIZE, false, false);
+    schedule_process(&process_table, &xpc_relay_event_loop_app);
 #endif
     return 0;
 }
